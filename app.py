4fa90b33d96f66b7fe7ac1a40385a5fdf8809f58
damachandra02 <137012168+damachandra02@users.noreply.github.com>
2025-03-07 15:49:28 +0530

    reduced size

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ def update_graph(date_idx, selected_vars):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run_server(debug=True, port=8067)
